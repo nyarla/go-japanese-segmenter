9aad32bf2486dec24ac206248ae27f157cae5757
nyarla <nyarla@thotep.net>
2019-02-25 10:15:25 +0900

    new: added documentation and example files

---
+++ _examples/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/nyarla/go-japanese-segmenter/defaults"
+	"github.com/nyarla/go-japanese-segmenter/segmenter"
+)
+
+func main() {
+	src := strings.NewReader("今日は良い天気ですね")
+	dst := new(strings.Builder)
+	dict := segmenter.BiasCalculatorFunc(defaults.CalculateBias)
+	seg := segmenter.New(dst, src)
+
+	for {
+		err := seg.Segment(dict)
+
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(dst.String())
+		dst.Reset()
+	}
+
+	fmt.Println(dst.String())
+	dst.Reset()
+
+	// Output:
+	// 今日
+	// は
+	// 良い
+	// 天気
+	// ですね
+}
---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd segmenter
+curl -LO http://www.genpaku.org/timemachine/timemachineu8j.txt
